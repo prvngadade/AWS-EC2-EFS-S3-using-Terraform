7ee24fc195388c68da7ffcbb38e5c89a991bbfdd
prvngadade <64361721+prvngadade@users.noreply.github.com>
2024-08-30 15:51:58 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,9 @@
+# Specific provider name according to the use case has to given!
+provider "aws" {
+  
+  # Write the region name below in which your environment has to be deployed!
+  region = "us-west2"
+
+  # Assign the profile name here!
+  profile = "default"
+}
